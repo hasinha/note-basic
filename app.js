a2e7401f49702a161701d6f1b1bb52949a65dbae
Bhaskar Sinha <hasinha@gmail.com>
2020-09-20 12:59:42 +0100

    changes

--- app.js
+++ app.js
@@ -1,76 +1,100 @@
+const request = require('request')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 80
 const fs = require('fs');
-const prompt = require('prompt-sync')();
 
-var action = prompt('Add(a)/Remove(r)/Display(d)?: ');
+app.get('/', (req, res) => {
+  var array = [];
+  var text = fs.readFileSync('notes.txt', 'utf8', function(err,data){
+      if (!err) {
+        return data;
+          // array = JSON.parse(data);
+          // console.log("parsed data: " + array);
+      } else {
+          console.log(err);
+      }
+  });
+  if(text){
+    console.log(text);
+    array = JSON.parse(text);
+  }
+  res.json(array);
+})
 
-var array = [];
-var text = fs.readFileSync('notes.txt', 'utf8', function(err,data){
-    if (!err) {
-      return data;
-        // array = JSON.parse(data);
-        // console.log("parsed data: " + array);
-    } else {
-        console.log(err);
+app.post('/', (req, res) => {
+  var title = req.query.title;
+  var body = req.query.body;
+
+  var array = [];
+  var text = fs.readFileSync('notes.txt', 'utf8', function(err,data){
+      if (!err) {
+        return data;
+          // array = JSON.parse(data);
+          // console.log("parsed data: " + array);
+      } else {
+          console.log(err);
+      }
+  });
+  if(text){
+    console.log(text);
+    array = JSON.parse(text);
+  }
+
+  var duplicate = array.find(note => note.id == title);
+  if(duplicate){
+    res.send('Duplicate')
+    return;
+  }
+  var sampleJson = {};
+  sampleJson['text'] = body;
+  sampleJson['id'] = title;
+  array.push(sampleJson);
+
+  fs.writeFile('notes.txt', JSON.stringify(array), function(err){
+    if(err){
+      console.log('Could not write to file. Error occurred: ', err);
+      res.send('Failed')
     }
-});
-if(text){
-  console.log(text);
-  array = JSON.parse(text);
-}
+    console.log('Written to file');
+  });
+  res.send('Success')
+})
 
-switch (action){
-  case 'a':
-    var id = prompt('Note id: ');
-    var text = prompt('Note Text: ');
-    var sampleJson = {};
-    sampleJson['text'] = text;
-    sampleJson['id'] = id;
-    array.push(sampleJson);
+app.delete('/', (req, res) => {
+  var title = req.query.title;
+  var array = [];
+  var text = fs.readFileSync('notes.txt', 'utf8', function(err,data){
+      if (!err) {
+        return data;
+          // array = JSON.parse(data);
+          // console.log("parsed data: " + array);
+      } else {
+          console.log(err);
+      }
+  });
+  if(text){
+    console.log(text);
+    array = JSON.parse(text);
+  }
 
+  var idx = array.map(function(e) { return e.id; }).indexOf(title);
+  if(idx != undefined && idx !== -1){
+    array.splice(idx, 1);
+    console.log(JSON.stringify(array));
     fs.writeFile('notes.txt', JSON.stringify(array), function(err){
       if(err){
         console.log('Could not write to file. Error occurred: ', err);
+        res.send('Failed')
       }
       console.log('Written to file');
     });
+    res.send('Success')
+  }else{
+    res.send('Not Found')
+  }
+})
 
-    break;
-  case 'r':
-    if(array.length < 1){
-      console.log('No notes found')
-      break;
-    }
-    var idx, i;
-    for(i=0;i<array.length;i++){
-      console.log(array[i]['id'] + ' : ' + array[i]['text'])
-      var toRemove = prompt('Remove(y)? : ')
-      if(toRemove === 'y'){
-        idx = i;
-        break;
-      }
-    }
-    if(idx != undefined){
-      array.splice(idx, 1);
-      console.log(JSON.stringify(array));
-      fs.writeFile('notes.txt', JSON.stringify(array), function(err){
-        if(err){
-          console.log('Could not write to file. Error occurred: ', err);
-        }
-        console.log('Written to file');
-      });
-    }
-
-    break;
-  case 'd':
-    if(array.length < 1){
-      console.log('No notes found');
-      break;
-    }
-    array.forEach((item, i) => {
-      console.log(item['id'] + ' : ' + item['text']);
-    });
-    break;
-  default:
-    console.log('Invalid action');
-    break;
-}
+app.listen(port, () => {
+  console.log('App listening on port : ', port);
+})
